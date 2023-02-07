5a15bba2ef33019533b5081dc6c2bb71e6da55f3
Darryllll <116327509+Darryllll@users.noreply.github.com>
2023-02-08 02:45:28 +0800

    some api thingy

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function LoginPage_Login(){
+/*function LoginPage_Login(){
   
   $(document).ready(function () {
     
@@ -36,7 +36,48 @@ function LoginPage_Login(){
     
     })
   });
-};
+};*/
 
+function submitButton(){
+  
+  $(document).ready(function () {
+    
+    const APIKEY = "63de48653bc6b255ed0c464c";
+      
+      let userName = $("#signUp-Email").val();
+      let userPass = $("#signUp-Password").val();
+      let userUname = $("#signUp-Username").val();
+      
+  
+      let jsondata = {
+        "email": userName,
+        "password": userPass,  
+        "username": userUname,
+      };
+      
+  
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://ipproject-81b0.restdb.io/rest/login",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata)
+      }
+      
+      
+      
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    
+    
+    })
+  });
+};
 
 
