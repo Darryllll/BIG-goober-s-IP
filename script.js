53d203a00560f611d052d19fdc3282c12de7f965
Darryllll <116327509+Darryllll@users.noreply.github.com>
2023-02-10 21:44:59 +0800

    dont poast when username/email taken

--- script.js
+++ script.js
@@ -38,25 +38,65 @@
   });
 };*/
 
+
+//for the sign up page
 function submitButton(){
-  
   $(document).ready(function () {
-    
     const APIKEY = "63de48653bc6b255ed0c464c";
-      
-      let userName = $("#signUp-Email").val();
-      let userPass = $("#signUp-Password").val();
-      let userUname = $("#signUp-Username").val();
-      
+    let userName = $("#signUp-Email").val();
+    let userPass = $("#signUp-Password").val();
+    let userUname = $("#signUp-Username").val();
   
-      let jsondata = {
-        "email": userName,
-        "password": userPass,  
-        "username": userUname,
-      };
-      
+    let jsondata = {
+      "email": userName,
+      "password": userPass,  
+      "username": userUname,
+    };
   
-      var settings = {
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://ipproject-81b0.restdb.io/rest/login",
+      "method": "GET",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(jsondata)
+    }
+  
+    $.ajax(settings).done(function (response) {
+      let emailTaken = false;
+      let usernameTaken = false;
+  
+      for (let i = 0; i < response.length; i++) {
+        if (response[i].email === userName) {
+          emailTaken = true;
+          break;
+        }
+  
+        if (response[i].username === userUname) {
+          usernameTaken = true;
+          break;
+        }
+      }
+  
+      if (emailTaken) {
+        $("#errorModal .modal-body").html("Error: email is already taken");
+        $("#errorModal").modal("show");
+        return;
+      }
+  
+      if (usernameTaken) {
+        $("#errorModal .modal-body").html("Error: username is already taken");
+        $("#errorModal").modal("show");
+        return;
+      }
+  
+      // Only make the POST request if both email and username are available
+      var postSettings = {
         "async": true,
         "crossDomain": true,
         "url": "https://ipproject-81b0.restdb.io/rest/login",
@@ -64,20 +104,17 @@ function submitButton(){
         "headers": {
           "content-type": "application/json",
           "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        },
+          "cache-control": "no-cache"},
         "processData": false,
         "data": JSON.stringify(jsondata)
       }
-      
-      
-      
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    
-    
-    })
+  
+      $.ajax(postSettings).done(function (postResponse) {
+        console.log(postResponse);
+      });
+    });
   });
 };
+
 
 
